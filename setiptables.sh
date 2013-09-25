2b279c472a2202661aa89f257bc48f3a32c7f8ee
yoo <yoo@yoo.hk>
2013-09-25 23:12:47 +0800

    touch setiptables.sh

---
+++ setiptables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum install -y iptables >>setiptables.log 2>&1
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 4399,53,12345,80 -j ACCEPT
+iptables -A INPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type ping -j DROP
+service iptables save
+service iptables restart
