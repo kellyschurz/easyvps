a46d5ada6e1baa615673952f2ed57e508ff03de6
yoo <yoo@yoo.hk>
2013-10-01 10:47:19 +0800

    modified

--- setiptables.sh
+++ setiptables.sh
@@ -15,7 +15,7 @@ iptables -A INPUT -p icmp --icmp-type ping -j DROP
 service iptables save
 service iptables restart
 echo "kern.warning /var/log/iptables.log" >>/etc/rsyslog.conf
-echo "*/5  *  *  *  * root /root/easyvps/noddos.sh" >>/etc/crontab
+echo "*/2  *  *  *  * root /root/easyvps/noddos.sh" >>/etc/crontab
 echo "*  */6  *  *  * root service iptables restart" >>/etc/crontab
 service iptables restart
 service crond restart
