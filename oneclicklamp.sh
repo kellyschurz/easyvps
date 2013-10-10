2e1d0ef329c86caca4384c0c1f9d607f91414100
yoo <yoo@yoo.hk>
2013-10-10 18:18:05 +0800

    modified oneclicklamp.sh

--- oneclicklamp.sh
+++ oneclicklamp.sh
@@ -11,5 +11,25 @@ LANG=C
 export PATH
 export LANG
 
-yum install httpd mysql mysql-server php php-mysql >/dev/null 2>&1
+OLDSERVERNAME="#ServerName www.example.com:80"
+NEWSERVERNAME="ServerName localhost:80"
+OLDINDEXES="Options Indexes FollowSymLinks"
+NEWINDEXES="Options -Indexes FollowSymLinks"
+HTTPDCONF="/etc/httpd/conf/httpd.conf"
 
+echo "install httpd mysql mysql-server php php-mysql"
+yum -y install httpd mysql mysql-server php php-mysql >/dev/null 2>&1
+echo "touch $1"
+mkdir /var/www/html/$1
+echo "setting httpd.conf"
+sed "s/$OLDSERVERNAME/$NEWSERVERNAME/g" -i $HTTPDCONF
+sed "s/$OLDINDEXES/$NEWINDEXES/g" -i $HTTPDCONF
+echo "NameVirtualHost *:80" >>$HTTPDCONF
+echo "<VirtualHost *:80>" >>$HTTPDCONF
+echo "DocumentRoot /var/www/html/$1" >>$HTTPDCONF
+echo "ServerName $2" >>$HTTPDCONF
+echo "</VirtualHost>" >>$HTTPDCONF
+echo "setting powerboot"
+echo "service httpd start" >>/etc/rc.local
+echo "httpd restart"
+service httpd restart
